384150de7cd2264f4afb700cbdfe5e1e8c9880ef
Daniel Grijuela <danielgrijuela@gmail.com>
2016-02-08 11:12:06 +0100

    Change email subject text

--- index.js
+++ index.js
@@ -102,10 +102,10 @@ let notify = (dependency, dependencyPackageJsonVersion, lastVersion, packageJson
 }
 
 let generateMessage = (dependency, dependencyPackageJsonVersion, lastVersion, packageJsonUrl, isMajor) => {
-  let grade = isMajor ? 'major' : 'minor';
+  let grade = isMajor ? 'MAJOR' : 'MINOR';
   return {
     'html': '<p>I have detected that in the package.json ' + packageJsonUrl + ' the dependency <b>' + dependency + '</b> has the version <b>' + dependencyPackageJsonVersion + '</b> selected and the last one available is the <b>' + lastVersion + '</b>.</p>' + '<p>Go and check out the last changes!: ' + npmUrl + dependency + '.</p>',
-    'subject': 'There is a ' + grade + ' release available for ' + dependency + ': ' + lastVersion,
+    'subject': '[' + grade + '] ' + dependency + ' ' + lastVersion,
     'from': process.env.SENDER_EMAIL,
     'fromname': process.env.SENDER_NAME
   }
